8188e7dbd9dbcceb906accc8c2d22e8d6972f45c
정호석 <chs1402@nexivil.com>
2019-04-26 16:54:03 +0900

    model init

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+# based on the which pizza option customer choose, different topping logic should be added.
+
 # Create your models here.
+class Regular_Pizza(models.Model):
+    name = models.CharField(max_length=30)
+    small = models.DecimalField(max_digits=100, decimal_places=2)
+    large = models.DecimalField(max_digits=100, decimal_places=2)
+
+
+class Sicilian_Pizza(models.Model):
+    name = models.CharField(max_length=30)
+    small = models.DecimalField(max_digits=100, decimal_places=2)
+    large = models.DecimalField(max_digits=100, decimal_places=2)
+
+
+class Toppings(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Subs(models.Model):
+    name = models.CharField(max_length=30)
+    small = models.DecimalField(max_digits=100, decimal_places=2)
+    large = models.DecimalField(max_digits=100, decimal_places=2)
+
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+
+
+class Salads:
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+
+
+class Dinner_Platters:
+    name = models.CharField(max_length=30)
+    small = models.DecimalField(max_digits=100, decimal_places=2)
+    large = models.DecimalField(max_digits=100, decimal_places=2)
