d4d636d75d53868d04ee59c689b45df915fd614f
unknown <kirklayer@gmail.com>
2019-05-06 10:49:08 +0900

    menuu

--- orders/urls.py
+++ orders/urls.py
@@ -5,11 +5,5 @@ from . import views
 
 app_name = 'orders'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("register_view", views.register_view, name="register_view"),
-    path("register_action", views.register_action, name="register_action"),
-    path("login_view", views.login_view, name="login_view"),
-    path("login_action", views.login_action, name="login_action"),
-    path("logout_action", views.logout_action, name="logout_action"),
     path("menu_view", views.menu_view, name="menu_view"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -5,54 +5,23 @@ from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import Regular_Pizza
+
 # Create your views here.
 #def index(request):
 #    return HttpResponse("Project 3: TODO")
 
 
 # Create your views here.
-def index(request):
-    context = {'hihi':9999}
-    return render(request, 'orders/index.html',context)
 
-def register_view(request):
-    context = {'hihi':9999}
-    return render(request, 'orders/register.html', context)
-
-def register_action(request):
-    if request.method == 'POST':
-        username = request.POST["username"]
-        password = request.POST["password"]
-        first = request.POST["first_name"]
-        last = request.POST["last_name"]
-        email = request.POST["email_address"]
-        if username and password and first and last and email:
-            print('savesavesaved')
-            user = User.objects.create_user(username,email,password)
-            user.save()
-            print(username)
-    return render(request, 'orders/index.html')
-
-def login_view(request):
-    context = {'hihi':9999}
-    return render(request, 'orders/login.html', context)
 
 def menu_view(request):
-    context = {'hihi':9999}
+
+    r = Regular_Pizza.objects.all()
+
+
+    print(r)
+
+    context = {'hihi':r}
     return render(request, 'orders/menu.html', context)
 
-def login_action(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        print('logged in!!')
-        return render(request, 'orders/index.html', {"message":"logged in!"})
-    else:
-        return render(request, 'orders/login.html', {"message":"invalid credential"})
-
-
-def logout_action(request):
-    logout(request)
-    return render(request, "orders/index.html", {"message":"Logged out."})
